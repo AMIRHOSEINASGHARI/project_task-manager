43b2722011feb3d7175c296f3fd52bde5ba1b74a
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2023-10-22 00:28:02 +0330

    securing register/login page

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,7 +1,22 @@
+"use client";
+
+//* Next
+import { useRouter } from "next/navigation";
 //* Components
 import { AuthForm } from "@/components";
+//* firebase
+import { auth } from "@/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useEffect } from "react";
 
 const LoginPage = () => {
+  const router = useRouter();
+  const [user, loading, error] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) router.push("/");
+  }, [user]);
+
   return <AuthForm type="login" />;
 };
 
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,7 +1,22 @@
+"use client";
+
+//* React
+import { useEffect } from "react";
+//* Next
+import { useRouter } from "next/navigation";
 //* Components
 import { AuthForm } from "@/components";
+//* firebase
+import { auth } from "@/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const RegisterPage = () => {
+  const router = useRouter();
+  const [user, loading, error] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) router.push("/");
+  }, [user]);
   return <AuthForm type="register" />;
 };
 
