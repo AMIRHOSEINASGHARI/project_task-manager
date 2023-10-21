2bb9d84685990544056828d6de8516204fe5bdc0
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2023-10-21 16:48:12 +0330

    initialize firebase app

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,3 +1,7 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
@@ -6,3 +10,9 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
 };
+
+const app = getApps.length === 0 ? initializeApp(firebaseConfig) : getApp();
+const firestore = getFirestore(app);
+const auth = getAuth(app);
+
+export { app, firestore, auth };
